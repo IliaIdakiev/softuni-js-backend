3f257229bcbed37d20f7bf5a507adffaae67138b
radrex <radostinv.stoychev@gmail.com>
2020-12-21 04:24:07 +0200

    Extract http actions, implement addBreed/addCat functionality

--- 1/resources/index.js
+++ 1/resources/index.js
@@ -4,12 +4,18 @@ const path = require('path');
 const fs = require('fs');
 const qs = require('querystring');
 
+//------ Third-Partu Modules ------
+const formidable = require('formidable');
+
 //--------- Local Modules ---------
 const config = require('./config.json');
 
 //----------- CONSTANTS -----------
 const VIEWS_PATH = path.resolve(config.viewsDir);
 const DATA_PATH = path.resolve(config.dataDir);
+const DATA_BREEDS_PATH = path.join(DATA_PATH, '/breeds.json');
+const DATA_CATS_PATH = path.join(DATA_PATH, '/cats.json');
+const CONTENT_PATH = path.resolve('content');
 
 const routeFileMap = {
   '/': '/home/index.html',
@@ -40,7 +46,7 @@ const actions = {
   GET: (req, res, pathname) => {
     if (pathname.includes('/content/')) { // For static files
       const fullStaticFilePath = path.resolve(pathname.slice(1));
-      sendFile(res, fullStaticFilePath);
+      sendFile(res, fullStaticFilePath, pathname);
       return;
     }
 
@@ -55,13 +61,12 @@ const actions = {
     }
   
     const fullFilePath = path.join(VIEWS_PATH, relativeFilePath);
-    sendFile(res, fullFilePath);
+    sendFile(res, fullFilePath, pathname);
   },
   POST: (req, res, pathname) => {
     if (pathname === '/addBreed') {
-      const fullFilePath = path.join(DATA_PATH, '/breeds.json');
       collectRequestData(req, result => {
-        fs.readFile(fullFilePath, function(err, data) {
+        fs.readFile(DATA_BREEDS_PATH, function(err, data) {
           let json;
           if (data.length !== 0) { // .json file is not empty
             json = JSON.parse(data)
@@ -70,7 +75,7 @@ const actions = {
             json = [result.breed];
           }
   
-          fs.writeFile(fullFilePath, JSON.stringify(json), (err) => {
+          fs.writeFile(DATA_BREEDS_PATH, JSON.stringify(json), (err) => {
             if (err) {
               console.log(err);
               return;
@@ -80,13 +85,35 @@ const actions = {
         res.writeHead(301, { location: '/' }).end();
       });
     } else if (pathname === '/addCat') {
-        const fullFilePath = path.join(DATA_PATH, '/cats.json');
-        //TODO implement addCat functionality
+      collectRequestData(req, result => {
+        fs.readFile(DATA_CATS_PATH, function(err, data) {
+          let json;
+          if (data.length !== 0) {
+            json = JSON.parse(data);
+            json.push({breed: result.breed, description: result.description, name: result.name, path: result.newPath, imageRelativePath: result.imageRelativePath });
+          } else {
+            json = [{breed: result.breed, description: result.description, name: result.name, path:result.newPath, imageRelativePath: result.imageRelativePath }];
+          }
+
+          //TODO: Check if file with the same name already exists in images, otherwise the code below will replace the old one.
+          fs.copyFile(result.oldPath, result.newPath, function(err) {
+            if (err) {
+              console.log(err);
+            }
+          });//TODO: Remove file from TEMP
+
+          fs.writeFile(DATA_CATS_PATH, JSON.stringify(json), (err) => {
+            console.log(err);
+            return;
+          });
+        });
+        res.writeHead(301, { location: '/' }).end();
+      });
     }
   }
 };
 
-function sendFile(res, fullFilePath) {
+function sendFile(res, fullFilePath, pathname) {
   const fileExt = path.extname(fullFilePath);
   const type = mimeTypeMap[fileExt];
 
@@ -100,15 +127,63 @@ function sendFile(res, fullFilePath) {
       return;
     }
 
-    res.writeHead(200, {
-      'Content-Length': Buffer.byteLength(data),
-      'Content-Type': type || 'text/plain'
-    }).end(data);
+    if (pathname === '/addCat') {
+      fs.readFile(DATA_BREEDS_PATH, function(err, buffer) {
+        let breeds;
+        if (buffer.length !== 0) {
+          breeds = JSON.parse(buffer);
+        } else {
+          breeds = [];
+        }
+
+        let catBreedPlaceholder = breeds.map(breed => `<option value="${breed}">${breed}</option>`).join('');
+        data = data.toString().replace('{{catBreeds}}', catBreedPlaceholder);
+        res.writeHead(200, {
+          'Content-Length': Buffer.byteLength(data),
+          'Content-Type': type || 'text/plain'
+        }).end(data);
+      });
+    } else if (pathname === '/') {
+      fs.readFile(DATA_CATS_PATH, function(err, buffer) {
+        let cats;
+        if (buffer.length !== 0) {
+          cats = JSON.parse(buffer);
+        } else {
+          cats = [];
+        }
+
+        let catsPlaceholder = cats.map(cat => `
+          <li>
+            <img src="${cat.imageRelativePath}" alt="${cat.name}">
+            <h3>${cat.name}</h3>
+            <p><span>Breed: </span>${cat.breed}</p>
+            <p><span>Description: </span>${cat.description}</p>
+            <ul class="buttons">
+              <li class="btn edit"><a href="">Change Info</a></li>
+              <li class="btn delete"><a href="">New Home</a></li>
+            </ul>
+          </li>
+        `).join('');
+
+        data = data.toString().replace('{{cats}}', catsPlaceholder);
+        res.writeHead(200, {
+          'Content-Length': Buffer.byteLength(data),
+          'Content-Type': type || 'text/plain'
+        }).end(data);
+      });
+    } else {
+      res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(data),
+        'Content-Type': type || 'text/plain'
+      }).end(data);
+    }
   });
 }
 
 function collectRequestData(req, callback) {
   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+  const FORM_MULTIPART = 'multipart/form-data';
+
   if (req.headers['content-type'] === FORM_URLENCODED) {
     let body = '';
     req.on('data', chunk => {
@@ -116,6 +191,21 @@ function collectRequestData(req, callback) {
     });
     req.on('end', () => {
       callback(qs.parse(body));
+    });
+  } else if (req.headers['content-type'].includes(FORM_MULTIPART)) {
+    let data = {};
+    const form = formidable({ multiples: true });
+    form.parse(req, (err, fields, files) => {
+      data.oldPath = files.upload.path;
+      data.newPath = `${CONTENT_PATH}/images/${files.upload.name}`;
+      data.imageRelativePath = `/content/images/${files.upload.name}`;
+      data.breed = fields.breed;
+      data.description = fields.description;
+      data.name = fields.name;
+    });
+
+    req.on('end', () => {
+      callback(data);
     });
   } else {
     callback(null);
